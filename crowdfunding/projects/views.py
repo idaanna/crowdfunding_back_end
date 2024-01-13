a8ec6e8ba8bb9a0a428b4ee12eb13549119de8e3
Ida Eriksson <annaida.eriksson@gmail.com>
2024-01-13 10:20:12 +0800

    project setup

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project
 from .serializers import ProjectSerializer
+from django.http import Http404
+from rest_framework import status
 
 class ProjectList(APIView):
 
@@ -14,4 +16,24 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+                )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+        
+class ProjectDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
